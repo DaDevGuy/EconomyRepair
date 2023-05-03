3278179050b93a2a6af29c6572e9c59b41406222
Rishi <129596573+Astro-PM@users.noreply.github.com>
2023-05-03 16:17:52 +0530

    Create Main.php

---
+++ src/dadevguy/EcoRepair/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace dadevguy\EcoRepair;
+
+use dadevguy\EcoRepair\Commands\RepairCommand;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+
+{
+
+    public function onEnable(): void
+
+    {
+
+        $this->getServer()->getCommandMap(new RepairCommand($this));
+
+        if($this->getConfig()->get("config-ver") !== 1)
+
+        {
+
+            $this->getLogger()->info("EconomyRepair! CONFIG Is Not Up To Date Please Delete config.yml and restart the server");
+
+        }
+
+        $this->saveDefaultConfig();
+
+    }
+
+}
